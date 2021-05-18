26abd61ec78fe9c2bedc1bca86bd6ce2e5c7a238
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-05-18 15:39:37 +0100

    first commit

---
+++ M3D2.js
@@ -0,0 +1,39 @@
+// window.onload = () => {
+//     fetchArtist()
+// }
+const fetchArtist = function(){
+
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "4f2b4fc7famsh93c76e8131cfb62p18069djsnba1591647707",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
+    })
+    .then((response) => response.json())
+    .then((artist) => {
+        console.log(artist[1])
+        
+        const row = document.getElementById('fetchAlbums')
+
+        for(let i=0; i < artist.length; i++){
+            const album = artist[i].album 
+            console.log(album)
+
+            const col = document.createElement('div')
+            col.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3')
+
+            const img = document.createElement('img')
+            img.classList.add('img-fluid')
+            img.src = album.cover_medium
+
+            col.appendChild(img)
+            row.appendChild(col)
+            
+        }
+    })
+    
+    .catch(err => {
+        console.error(err);
+    });
+}
