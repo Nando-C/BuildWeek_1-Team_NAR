87a35ee7f15c3040e5af02fd1be8c85c2fa6f791
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-05-18 17:59:36 +0100

    created cards on JS
    created cards on JavaScript, populated content from fetched information and added them to the Artist page (M3D2)

--- M3D2.js
+++ M3D2.js
@@ -1,6 +1,8 @@
-// window.onload = () => {
-//     fetchArtist()
-// }
+window.onload = () => {
+    fetchArtist()
+}
+const row = document.getElementById('fetchAlbums')
+
 const fetchArtist = function(){
 
     fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem", {
@@ -14,7 +16,7 @@ const fetchArtist = function(){
     .then((artist) => {
         console.log(artist)
         
-        const row = document.getElementById('fetchAlbums')
+        // const row = document.getElementById('fetchAlbums')
 
         for(let i=0; i < artist.data.length; i++){
             const album = artist.data[i].album 
@@ -23,11 +25,43 @@ const fetchArtist = function(){
             const col = document.createElement('div')
             col.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'mb-4')
 
+            const card = document.createElement('div')
+            card.classList.add('card', 'h-100')
+
+            const linkCard = document.createElement('a')
+            linkCard.href="album.html"
+
             const img = document.createElement('img')
-            img.classList.add('img-fluid')
+            img.classList.add('card-img-top')
             img.src = album.cover_medium
 
-            col.appendChild(img)
+            const cardPlayIcon = document.createElement('div')
+            cardPlayIcon.classList.add('card-play')
+
+            const cardBody = document.createElement('div')
+            cardBody.classList.add('card-body', 'text-center', 'p-3')
+
+            const albumName = document.createElement('p')
+            albumName.classList.add('card-text', 'text-white', 'mb-0', 'mt-1')
+            albumName.innerText = album.title
+
+            const artistName = document.createElement('p')
+            artistName.classList.add('card-text', 'text-muted')
+            artistName.innerHTML = `<small class="text-muted"><strong>Queen</strong></small>`
+            
+            linkCard.appendChild(img)
+
+            card.appendChild(linkCard)
+            card.appendChild(cardPlayIcon)
+
+            cardBody.appendChild(albumName)
+            cardBody.appendChild(artistName)
+
+            card.appendChild(cardBody)
+
+            col.appendChild(card)
+
+            // col.appendChild(img)
             row.appendChild(col)
             
         }
@@ -36,4 +70,9 @@ const fetchArtist = function(){
     .catch(err => {
         console.error(err);
     });
+}
+
+const listAlbums = function(){
+    row.classList.toggle('d-none')
+    console.log(row)
 }
